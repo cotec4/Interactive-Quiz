a0fa6849cab4bce0401cd8bc4c7c3a7f6767c206
Connor Cote <cotec4@gmail.com>
2020-07-09 22:14:45 -0400

    fully operational but missing some styling

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var startQuiz = document.getElementById("start-button");
 var quizBody = document.getElementById("quiz-body");
 var introBody = document.getElementById("intro-body");
 var seconds = document.getElementById("seconds");
+var highScoresDiv = document.getElementById("highscores");
+var butttonsDiv = document.getElementById("buttons");
 var i = 0;
 var score = 0;
 var secs = 76;
@@ -75,26 +77,24 @@ function countdown() {
     interval = setInterval(function () {
         if (secs === 0) {
             clearInterval(interval);
-            console.log(secs);
             stopQuiz();
-            // Stop quiz and send to input initials and submit
         }
         secs--;
         seconds.textContent = secs;
     }, 1000)
 }
 
-$(startQuiz).on("click", function() {
-    buildQuiz;
+$(startQuiz).on("click", function () {
+    buildQuiz();
     countdown();
 })
 
 function buildQuiz() {
+    introBody.setAttribute("style", "display: none");
     if (quizBody.style.display === "none") {
-        quizBody.setAttribute("style", "display: inline-block");
+        $(quizBody).attr("style", "display: inline-block");
     }
     i = 0;
-    introBody.setAttribute("style", "display: none");
     $(question).text(questions[i].Question);
     $(answer0).text(questions[i].answers.response1);
     $(answer1).text(questions[i].answers.response2);
@@ -118,7 +118,6 @@ function buildQuiz() {
 function setNextQuestion() {
     if (questions.length <= i) {
         stopQuiz();
-        console.log(secs);
         clearInterval(interval);
     }
     else {
@@ -138,27 +137,43 @@ function stopQuiz() {
     $("#result").text("Your Score Is: " + score);
     $("<h5>").text("Finished! Congrats on learning more about Atlanta!").appendTo("#quiz-body");
     var initialsInput = $("<input>").attr("type", "text").attr("placeholder", "Please insert your initials");
-    var submtiBtn = $("<button>").attr("type", "submit").text("Submit").attr("id","submitBtn");
+    var submtiBtn = $("<button>").attr("type", "submit").text("Submit").attr("id", "submitBtn");
     $("#quiz-body").append(initialsInput, submtiBtn);
     $(submtiBtn).on("click", function () {
-        localStorage.setItem($("input").val(), score);
+        var initials = $("input").val();
+        localStorage.setItem(initials, score);
+        highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+        var endScore = {
+            finalScore: score,
+            initials: initials
+        };
+        highscores.push(endScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
         window.location.href = "./leaderboard.html";
     })
 }
 
-var array = Object.keys(localStorage);
-for (var i = 0; i < array.length; i++) {
-    localStorage[array[i]];
-    // provides the initials
-    console.log(array[i]);
-    // provides the value
-    console.log(localStorage[array[i]]);
-    console.log(i);
-    $("<div>").attr("id","standings").appendTo("div");
-    $("<textarea>").attr("id","firstPlace").text("1." + " " + array[i] + " " + localStorage[array[i]]).appendTo("div");
-    // Store all scores in local storage and work on HTML to show top scores (sorting from best to worst score)
+function showHighScores() {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    console.log(typeof (highscores));
+    highscores.sort(function (a, b) {
+        return b.finalScore - a.finalScore
+    });
+    console.log(highscores);
+    for (var i = 0; i < highscores.length; i++) {
+        var showScores = $("<div>").attr("class", "highScores");
+        showScores.text(highscores[i].initials + " " + highscores[i].finalScore);
+        showScores.appendTo(highScoresDiv);
+    }
     var goBackBtn = $("<button>").attr("id", "goBackBtn").text("Go Back");
-    var clearHighScores = $("<button>").attr("id","clearHighScores").text("Clear Highscores");
-    var buttons = $("<div>");
-    $(buttons).append(goBackBtn, clearHighScores);
+    $(goBackBtn).on("click", function () {
+        window.location.href = "./index.html"
+    })
+    var clearHighScores = $("<button>").attr("id", "clearHighScores").text("Clear Highscores");
+    $(clearHighScores).on("click", function () {
+        localStorage.clear();
+        $(".highScores").text("");
+        return
+    })
+    $(butttonsDiv).append(goBackBtn, clearHighScores);
 }
